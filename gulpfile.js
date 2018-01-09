ce1b96443db793e1163a21679b33005e7b40ed29
Naggi <nagase2@gmail.com>
2018-01-09 23:40:52 +0900

    minify追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,43 @@
 var gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', defaultTask);
 
+gulp.task('default', ()=>
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+);
+
+// copy Html
+gulp.task('copyHTML',function(){
+  gulp.src('src/*.html')
+  .pipe(gulp.dest('dist'))
+});
+
+
+// Minify JS
+gulp.task('minify',function(){
+  gulp.src('src/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
+});
+
+
+
+
+
+
+
+//------------------
 function defaultTask(done) {
   // place code for your default task here
   console.log("default task executed. ")
   done();
 }
+
+gulp.task('message',function(){
+  return console.log('Gulp is runnning...')
+});
+
 
---
+++ src/js/file1.js
@@ -0,0 +1,10 @@
+//コンソール
+console.log('message is 1');
+
+console.log('message is 2');
+
+sample('value');
+
+function sample(sampleValue1){
+    console.log(sampleValue1);
+}
