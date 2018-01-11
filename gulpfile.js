9a6c5900b392aa5b41fe8e66ceedef44a46e9a87
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-01-11 14:09:40 +0900

    livereload added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const less = require('gulp-less');
+const livereload = require('gulp-livereload');
 
 
 gulp.task('default', ()=>
@@ -12,7 +14,9 @@ gulp.task('default', ()=>
 // copy Html
 gulp.task('copyHTML',function(){
   gulp.src('src/*.html')
-  .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest('dist'));
+  //.pipe(gulp.livereload()))
+
 });
 
 
@@ -24,9 +28,17 @@ gulp.task('minify',function(){
 });
 
 
+gulp.task('less', function() {
+gulp.src('less/*.less')
+.pipe(less())
+.pipe(gulp.dest('css'))
+.pipe(livereload());
+});
 
-
-
+gulp.task('watch', function() {
+livereload.listen();
+gulp.watch('less/*.less', ['less']);
+});
 
 
 //------------------
