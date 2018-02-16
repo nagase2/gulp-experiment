3a66bbd45a82e29616910b8bb26ddd1775a4a581
yoshimasa nagase <yoshimasa_nagase@tsyscom.co.jp>
2018-02-16 18:42:42 +0900

    call fakeを追加した

---
+++ spec/spy-face-test.spec.js
@@ -0,0 +1,14 @@
+
+
+
+
+describe("The 'toEqual' matcher", function() {
+
+    it("works for simple literals and variables", function() {
+      var a = 12;
+      expect(a).toEqual(12);
+
+      
+    });
+})
+
