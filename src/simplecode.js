90ea0d84373b0f1880d246c0c92e18b3ba3b5f9b
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-02-23 14:30:23 +0900

    commanderのデモを追加した。

---
+++ src/simplecode.js
@@ -0,0 +1,13 @@
+
+
+
+console.log("ssss")
+var itemName = "hee"
+var volume = {foo:"foo value",
+hee:"var value",
+shoo:"shoo value"}
+
+//下記3つの記法は同じ意味となる。
+console.log(volume["hee"])
+console.log(volume.hee)
+console.log(volume[itemName])
