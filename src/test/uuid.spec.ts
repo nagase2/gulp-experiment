0eed11f9705133953984b569fc4692ea0d195a66
yoshimasa_nagse <yoshimasa_nagase@ss.toyota-tsusho.com>
2018-06-15 19:50:17 +0900

    ts変換実施

--- src/test/uuid.spec.ts
+++ src/test/uuid.spec.ts
@@ -1,8 +1,9 @@
-const _ = require("underscore")
+import _ = require("underscore")
 
-const fs = require('fs')
-
-
+import fs = require('fs')
+import uuidv1 = require("uuid/v1")
+import ShortUniqueId = require("short-unique-id")
+import "jasmine"
 
 
 
@@ -18,7 +19,7 @@ describe("UUIDの作成テスト", () => {
   })
 
   it("UUIDを取得するサンプル", () => {
-    const uuidv1 = require("uuid/v1")
+
     uuidv1() /*?*/
     uuidv1()/*?*/
     uuidv1()/*?*/
@@ -27,21 +28,21 @@ describe("UUIDの作成テスト", () => {
     uuidv1()/*?*/
     // Instantiate
     // Import
-    var ShortUniqueId = require("short-unique-id")
+
     const uid = new ShortUniqueId()
     uid.randomUUID(6)
     uid.randomUUID(8)
     uid.randomUUID(8)
   })
   it("関係ないけどオブジェクトの動作確認", () => {
-    var value1 = { foo:"xxx", bar:"cccc"}
-    
+    var value1 = { foo: "xxx", bar: "cccc" }
+
     value1.mm = "nagase"
 
     console.log(value1)
-  
-    
-    
+
+
+
   })
 
 })
